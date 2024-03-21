330c8cb59686fc3427738efcf4f4beeb43520880
acpiccolo <60399193+acpiccolo@users.noreply.github.com>
2024-03-21 12:38:39 +0100

    fix: added gtk and adw initialization

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,15 @@ pub extern crate relm4_components;
 pub mod gtk;
 mod localization;
 
-pub fn init() {
+pub fn init() -> anyhow::Result<()> {
     localization::init();
+    relm4::gtk::init()?;
+    relm4::adw::init()?;
     #[cfg(feature = "third_party_licenses_dialog")]
-    relm4_icons::initialize_icons();
+    {
+        relm4_icons::initialize_icons();
+    }
+    Ok(())
 }
 
 #[cfg(feature = "third_party_licenses_dialog")]
